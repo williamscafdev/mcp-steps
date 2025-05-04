45e0b4f332c1b65614ed9f4b791741d3215a988b
williams <wmtorresb@gmail.com>
2025-05-04 18:47:58 -0500

    feat: add fetch-product-by-title tool to retrieve product by title
    This commit introduces a new tool `fetch-product-by-title` that allows fetching product details by providing the product title. The tool uses Zod for input validation and returns the product details if found, otherwise, it returns a "Product not found" message.

--- products/src/index.ts
+++ products/src/index.ts
@@ -1,5 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
 
 const server = new McpServer({
     name: "products",
@@ -48,6 +49,65 @@ server.tool("fetch-product", "Get product model", async (input) => {
         ]
     }
 });
+
+server.tool("fetch-product-by-title", "Get product model by id",{
+    title: z.string().describe("Title of the product"),
+}, async (title) => {
+    const product = [
+        {
+            "title": "Iphone 14",
+            "brand": "Apple",
+            "model": "Iphone 14",
+            "color": "red",
+        },
+        {
+            "title": "Iphone 14 Pro",
+            "brand": "Apple",
+            "model": "Iphone 14 Pro",
+            "color": "black",
+        },
+        {
+            "title": "Iphone 13 Pro Max",
+            "brand": "Apple",
+            "model": "Iphone 13 Pro Max",
+            "color": "yellow",
+        },
+        {
+            "title": "Iphone 12 Pro Max",
+            "brand": "Apple",
+            "model": "Iphone 12 Pro Max",
+            "color": "green",
+        },
+        {
+            "title": "Iphone 14 Pro Max",
+            "brand": "Apple",
+            "model": "Iphone 14 Pro Max",
+            "color": "white",
+        },
+    ];
+
+    for (let i = 0; i < product.length; i++) {
+        if (product[i].title === title.title) {
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: JSON.stringify(product[i]),
+                    }
+                ]
+            }
+        }
+    }
+
+    return {
+        content: [
+            {
+                type: "text",
+                text: "Product not found",
+            }
+        ]
+    }
+})
 
 const transport = new StdioServerTransport();
 await server.connect(transport);
